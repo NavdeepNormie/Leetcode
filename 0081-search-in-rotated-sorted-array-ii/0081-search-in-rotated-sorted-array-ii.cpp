46ddfe2ed4e751cecf159c4ac604f91bc573bc92
Navdeep <151021272+NavdeepNormie@users.noreply.github.com>
2026-07-15 18:42:01 +0530

    Time: 0 ms (100%), Space: 18.1 MB (45.23%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+
+        int low = 0;
+        int high = nums.size() - 1;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            // Target found
+            if (nums[mid] == target)
+                return true;
+            if (nums[low] == nums[mid] && nums[mid] == nums[high]){
+                low ++, high --;
+                continue;
+            }
+
+            // Check if the left half is sorted
+            if (nums[low] <= nums[mid]) {
+
+                // Target lies in the left half
+                if (nums[low] <= target && target < nums[mid]) {
+                    high = mid - 1;
+                }
+                // Target lies in the right half
+                else {
+                    low = mid + 1;
+                }
+            }
+
+            // Right half is sorted
+            else {
+
+                // Target lies in the right half
+                if (nums[mid] < target && target <= nums[high]) {
+                    low = mid + 1;
+                }
+                // Target lies in the left half
+                else {
+                    high = mid - 1;
+                }
+            }
+        }
+
+        return false;
+    }
+};
