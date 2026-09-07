6a3b3b67970bf8a1bb38ea6e388b76a56d39df49
Navdeep <151021272+NavdeepNormie@users.noreply.github.com>
2026-09-07 17:59:20 +0530

    Time: 4 ms (86.17%), Space: 45 MB (11.93%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        int left = 0;
+        int right = 0;
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+        while(left<s.size() && right<g.size()){
+            if(g[right] <= s[left]){
+                right = right + 1;
+            }
+        left = left + 1;
+        }
+    return right;
+    }
+};
